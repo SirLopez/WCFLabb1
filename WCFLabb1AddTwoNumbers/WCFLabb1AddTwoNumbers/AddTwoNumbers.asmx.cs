b0afd957e39a9b69c0155e8ea43e6134839792ea
SirLopez <daniel.lopez@live.se>
2016-01-22 11:46:02 +0100

    Added service for 2 numbers

---
+++ WCFLabb1AddTwoNumbers/WCFLabb1AddTwoNumbers/AddTwoNumbers.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WCFLabb1AddTwoNumbers
+{
+    /// <summary>
+    /// Summary description for AddTwoNumbers
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class AddTwoNumbers : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public int Add2Numbers(string x, string y)
+        {
+            return int.Parse(x) + int.Parse(y);
+        }
+    }
+}
